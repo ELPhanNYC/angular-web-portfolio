7dcb655effaf3e8495dc854b86610d192fb485ab
elphan <starsoccerethan@gmail.com>
2023-08-07 11:02:17 -0400

    address repetitiveness of footer

--- portfolio/src/app/footer/footer.component.ts
+++ portfolio/src/app/footer/footer.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  constructor(){
+
+  }
+
+  links = [{icon:"../assets/images/icons8-github-50.png", href:"https://github.com/ELPhanNYC", title:"GitHub"}, 
+          {icon:"../assets/images/icons8-linkedin-50.png", href:"https://www.linkedin.com/in/ethanlphan/", title:"Linkedin"}, 
+          {icon:"../assets/images/icons8-resume-30.png", href:"", title:"Resume"}];
 
 }
