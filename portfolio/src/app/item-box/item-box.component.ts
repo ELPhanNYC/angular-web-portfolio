83dd724ae28f888a5d4f945b3c5cb4d851f9c9ac
elphan <starsoccerethan@gmail.com>
2023-08-07 10:27:27 -0400

    Completed base homepage

--- portfolio/src/app/item-box/item-box.component.ts
+++ portfolio/src/app/item-box/item-box.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-box.component.scss']
 })
 export class ItemBoxComponent {
+  constructor(){
+
+  }
+  icons = [{src:"../../assets/icons/python_logo.png"},{src:"../../assets/icons/shorter_java_logo.png"},{src:"../../assets/icons/scala_logo.png"},
+  {src:"../../assets/icons/c_logo.png"},{src:"../../assets/icons/csharp_logo.png"},{src:"../../assets/icons/angular_logo.png"},{src:"../../assets/icons/scss_logo.png"},
+  {src:"../../assets/icons/ts_logo.png"},{src:"../../assets/icons/html_logo.png"},{src:"../../assets/icons/css_logo.png"},{src:"../../assets/icons/js_logo.webp"},
+  {src:"../../assets/icons/git_logo.png"},{src:"../../assets/icons/ci-cd.png"},{src:"../../assets/icons/docker_logo.webp"},{src:"../../assets/icons/net_logo.png"},
+  {src:"../../assets/icons/bottle_icon_w:o_bg.png"},{src:"../../assets/icons/sqlite-square-icon.png"}];
 
 }
