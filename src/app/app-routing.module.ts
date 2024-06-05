fadaef6df22dfbc40e6ffde3aaf9e2a12be0c514
elphan <starsoccerethan@gmail.com>
2024-06-05 12:13:31 -0400

    Re-init project directory...manual

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ExperienceComponent } from './experience/experience.component';
 
-const routes: Routes = [{path:'app-home', component: HomeComponent, title: 'Ethan Phan'},{path:'app-experience', component: ExperienceComponent, title: 'Experience'}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,53 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AboutCardComponent } from './about-card/about-card.component';
-import { SlideshowComponent } from './slideshow/slideshow.component';
-import { ItemBoxComponent } from './item-box/item-box.component';
-import { SmoothScrollDirective } from './smooth-scroll.directive';
-import { MenuComponent } from './menu/menu.component';
-import { EducationComponent } from './education/education.component';
+
 
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule }  from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule} from '@angular/material/tabs';
-import { CarouselComponent } from './carousel/carousel.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent,
     HomeComponent,
-    ExperienceComponent,
-    AboutCardComponent,
-    SlideshowComponent,
-    ItemBoxComponent,
-    SmoothScrollDirective,
-    MenuComponent,
-    EducationComponent,
-    CarouselComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     MatCardModule,
     MatMenuModule,
     MatIconModule,
     MatExpansionModule,
     MatTabsModule,
-    
   ],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [SmoothScrollDirective]
+  exports: []
 })
 export class AppModule { }
