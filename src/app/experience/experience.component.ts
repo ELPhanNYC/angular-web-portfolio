1a66bbc74736bd99abc3700fa54bb897cc2432a2
elphan <starsoccerethan@gmail.com>
2024-06-06 15:12:50 -0400

    added cascading fade and fixed some minor issues

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { fadeInAnimation } from 'src/assets/animations/animations';
+import { fadeInAnimation, cascadeFade1, cascadeFade2, cascadeFade3, cascadeFade4 } from 'src/assets/animations/animations';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.scss'],
-  animations: [fadeInAnimation]
+  animations: [fadeInAnimation, cascadeFade1, cascadeFade2, cascadeFade3, cascadeFade4]
 })
 export class ExperienceComponent {
 
